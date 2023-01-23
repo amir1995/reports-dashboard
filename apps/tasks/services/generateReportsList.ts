a370ccb861c3ac74ac4a597d0ba9c21c76251cd9
Amir Hosein <amirhoseinhesarakii@gmail.com>
2023-01-23 16:45:30 +0330

    [modify] reformat the date object

--- apps/tasks/services/generateReportsList.ts
+++ apps/tasks/services/generateReportsList.ts
@@ -3,6 +3,12 @@ import { TBodyGenerateReports } from '@/types/types/serviceCallTypes';
 import apiClient from '@/utils/apiClient';
 
 export const reports = (data: TBodyGenerateReports): any => {
+
+  const normalWayToFormatDate = (date:string): string => {
+    // BTW we can use moment js for formatting the date object
+    // return moment(date).format('dd.MM.YYYY');
+    return date.split('-').reverse().toString().replaceAll(',', '.')
+  }
   return apiClient<IReportsData>({ url: '/api/generateReportsList/', method: 'post', data }).then(
     res => {
       let normalizeData = {};
@@ -32,12 +38,25 @@ export const reports = (data: TBodyGenerateReports): any => {
           if (acc[cur.projectId]) {
             acc = {
               ...acc,
-              [cur.projectId]: [...acc[cur.projectId], cur],
+              [cur.projectId]: [
+                ...acc[cur.projectId],
+                {
+                  ...cur,
+                  created: normalWayToFormatDate(cur.created),
+                  modified: normalWayToFormatDate(cur.modified),
+                },
+              ],
             };
           } else {
             acc = {
               ...acc,
-              [cur.projectId]: [cur],
+              [cur.projectId]: [
+                {
+                  ...cur,
+                  created: normalWayToFormatDate(cur.created),
+                  modified: normalWayToFormatDate(cur.modified),
+                },
+              ],
             };
           }
           return acc;
